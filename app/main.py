8f09951f6973bec462b31da8353f597a94c2264f
manantshah <manantshah97@gmail.com>
2026-05-02 08:14:30 -0700

    chore/Inbound_Emails-add-system_logged_at

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def read_inbound(inbound_email : Inbound):
     db_conn = get_db_connection()
 
     cur = db_conn.cursor()
-    cur.execute("INSERT INTO Inbound_Emails (customer_email, support_email, subject_line, email_body, email_received_at, GAS_fired_at) VALUES (%s, %s, %s, %s, %s, %s) RETURNING id;", (inbound_email.customer_email, inbound_email.support_email, inbound_email.subject_line, inbound_email.email_body, inbound_email.email_received_at, inbound_email.GAS_fired_at))
+    cur.execute("INSERT INTO Inbound_Emails (customer_email, support_email, subject_line, email_body, email_received_at, GAS_fired_at, system_logged_at) VALUES (%s, %s, %s, %s, %s, %s, NOW()) RETURNING id;", (inbound_email.customer_email, inbound_email.support_email, inbound_email.subject_line, inbound_email.email_body, inbound_email.email_received_at, inbound_email.GAS_fired_at))
     returned_id = cur.fetchone()[0]
 
     ai_json_response = analyze_email(inbound_email.subject_line, inbound_email.email_body)
